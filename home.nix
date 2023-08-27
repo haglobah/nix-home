a876c592d878af7b69c3c1cc16ff86350feaa6c4
Beat Hagenlocher <bah@posteo.de>
2023-08-27 19:21:18 +0200

    Add Obsidian & Discord

--- home.nix
+++ home.nix
@@ -15,12 +15,19 @@
   # release notes.
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowUnfreePredicate = (_: true);
+  };
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    obsidian
+    discord
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
