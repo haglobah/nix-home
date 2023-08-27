0e1838f1fbd012959a5ba1049d1d14c159e511b3
Beat Hagenlocher <bah@posteo.de>
2023-08-27 21:44:13 +0200

    Add starship

--- home.nix
+++ home.nix
@@ -111,6 +111,21 @@
     };
   };
 
+  programs.starship = {
+    enable = true;
+    # Configuration written to ~/.config/starship.toml
+    settings = {
+      # add_newline = false;
+
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+
+      # package.disabled = true;
+    };
+  };
+
   programs.git = {
     userEmail = "bah@posteo.de";
     userName = "Beat Hagenlocher";
