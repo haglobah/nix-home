871f2fd5637c308fa1e195e48187c8c7b81851b5
Beat Hagenlocher <bah@posteo.de>
2023-08-27 23:00:25 +0200

    Add tmux

--- home.nix
+++ home.nix
@@ -28,6 +28,7 @@
     # pkgs.hello
     obsidian
     discord
+    tmux
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
