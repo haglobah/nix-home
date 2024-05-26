fa667d3af96ed53c6bbdbc03bce6857a16178486
Beat Hagenlocher <bah@posteo.de>
2024-05-26 17:35:44 +0200

    Add registries to user registry

--- home.nix
+++ home.nix
@@ -22,6 +22,42 @@
     ];
   };
 
+  nix.registry = {
+    my = {
+      from = {
+        id = "my";
+        type = "indirect";
+      };
+      to = {
+        owner = "haglobah";
+        repo = "flakes";
+        type = "github";
+      };
+    };
+    n = {
+      from = {
+        id = "n";
+        type = "indirect";
+      };
+      to = {
+        type = "tarball";
+        url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*.tar.gz";
+      };
+    };
+    nu = {
+      from = {
+        id = "nu";
+        type = "indirect";
+      };
+      to = {
+        type = "github";
+        owner = "nixos";
+        repo = "nixpkgs";
+        ref = "nixos-unstable";
+      };
+    };
+  };
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
