dc9aaf0d9a21c53cbbafd5dd1ca03d1a7af9d981
Beat Hagenlocher <bah@posteo.de>
2024-12-18 17:40:21 +0100

    Disable idle saving

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -51,7 +51,6 @@
 
 (after! super-save
   (super-save-mode +1)
-  (setq super-save-auto-save-when-idle t)
   (setq auto-save-default nil)
   (setq auto-save-remote-files nil)
   (setq super-save-silent t)
